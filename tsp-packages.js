a87b6b87d8b7bb930389010e989239856bbc45ff
Mike Harder <mharder@microsoft.com>
2023-11-09 03:09:14 +0000

    [tsp-packages] Convert from sh to js

---
+++ tsp-packages.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const packages = [
+    "@azure-tools/typespec-apiview",
+    "@azure-tools/typespec-autorest",
+    "@azure-tools/typespec-azure-core",
+    "@azure-tools/typespec-autorest",
+    "@azure-tools/typespec-azure-core",
+    "@azure-tools/typespec-azure-resource-manager",
+    "@azure-tools/typespec-client-generator-core",
+    "@azure-tools/typespec-providerhub",
+    "@typespec/compiler",
+    "@typespec/http",
+    "@typespec/openapi",
+    "@typespec/rest",
+    "@typespec/versioning",
+];
+
+async function getPackageVersion(packageName) {
+    const { stdout } = await exec(`npm view ${packageName} version`);
+    return stdout.trim();
+}
+
+async function printVersionsInOrder() {
+    const promises = packages.map(async (package) => {
+        const version = await getPackageVersion(package);
+        return { package, version };
+    });
+
+    const results = await Promise.all(promises);
+
+    results.forEach(({ package, version }) => {
+        console.log(`${package}: ${version}`);
+    });
+}
+
+printVersionsInOrder().catch(console.error);
