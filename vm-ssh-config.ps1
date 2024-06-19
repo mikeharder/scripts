796bb79af85f667029df768f759756cb8b89b99a
Mike Harder (from Dev Box) <mharder@microsoft.com>
2024-06-19 11:58:52 -0700

    Update VM name

--- vm-ssh-config.ps1
+++ vm-ssh-config.ps1
@@ -1,6 +1,6 @@
 $ip = Get-NetNeighbor -LinkLayerAddress 00-15-5d-*
 
-$content = "Host mharder-dev-u22" + [Environment]::NewLine + `
+$content = "Host mharder-dev-u24" + [Environment]::NewLine + `
            "  HostName " + $ip.IPAddress + [Environment]::NewLine + `
            "  User mharder" + [Environment]::NewLine;
 
