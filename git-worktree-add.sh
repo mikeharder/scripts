f648321c8e6477aab16476971ad74d40cc46665b
Mike Harder <mharder@microsoft.com>
2026-04-20 20:50:27 -0700

    scripts to add/remove worktrees (#13)

---
+++ git-worktree-add.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <branch-name>" >&2
+    exit 1
+fi
+
+name=$1
+
+git worktree add --no-checkout -b "$name" "../$name"
+cd "../$name"
+git sparse-checkout init --cone
+git sparse-checkout set .github/actions .github/shared .github/workflows
+git checkout
